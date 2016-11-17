0eee9e9e3d1e61272370009a772d34d8270e38ec
meg-murphy <meghan.murphy.usc@gmail.com>
2016-11-16 21:31:23 -0800

    add pretty_time method to update show page

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,11 @@
 class Contact < ApplicationRecord
+
+  def pretty_time
+    return created_at.strftime("%A, %d %b %Y %l:%M %p")
+  end
+
+  # def full_name
+  #   return .first_name.last_name
+  # end
+
 end
